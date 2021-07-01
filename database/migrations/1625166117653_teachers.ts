1aad1a70a3d9a7da41793939bdd3598a0dad3502
carloseduardodb <carloscangere@hotmail.com>
2021-07-01 18:17:13 -0300

    feat(teacher.ts): add new migration for model Teacher

---
+++ database/migrations/1625166117653_teachers.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Teachers extends BaseSchema {
+  protected tableName = 'teachers'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('name')
+      table.string('email').notNullable().unique()
+      table.string('password')
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
