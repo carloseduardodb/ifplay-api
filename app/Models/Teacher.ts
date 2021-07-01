b7025703a05e1f50f3e70bac0338875912ec8666
carloseduardodb <carloscangere@hotmail.com>
2021-07-01 18:25:54 -0300

    feat: add model teacher

--- app/Models/Teacher.ts
+++ app/Models/Teacher.ts
@@ -1,13 +1,28 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import Playlist from './Playlist'
 
 export default class Teacher extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public email: string
+
+  @column()
+  public password: string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @hasMany(() => Playlist, {
+    foreignKey: 'teacherId',
+  })
+  public playlist: HasMany<typeof Playlist>
 }
