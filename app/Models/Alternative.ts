b51a164e5ab958c4886f706f64b63d7f4977bc07
carloseduardodb <carloscangere@hotmail.com>
2021-07-01 18:27:29 -0300

    feat: add model alternative

--- app/Models/Alternative.ts
+++ app/Models/Alternative.ts
@@ -1,13 +1,25 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, HasOne, hasOne } from '@ioc:Adonis/Lucid/Orm'
+import Question from './Question'
 
 export default class Alternative extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public questionId: number
+
+  @column()
+  public isResponse: boolean
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @hasOne(() => Question, {
+    localKey: 'questionId',
+  })
+  public question: HasOne<typeof Question>
 }
