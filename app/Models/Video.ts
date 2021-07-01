6148cf43b28378fb41e7be96a2433a24f792854d
carloseduardodb <carloscangere@hotmail.com>
2021-07-01 18:25:41 -0300

    feat: add model video

--- app/Models/Video.ts
+++ app/Models/Video.ts
@@ -1,13 +1,28 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, HasMany, HasOne, hasOne } from '@ioc:Adonis/Lucid/Orm'
+import Playlist from './Playlist'
 
 export default class Video extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public playlistId: number
+
+  @column()
+  public url: string
+
+  @column()
+  public views: number
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @hasOne(() => Playlist, {
+    localKey: 'playlistId',
+  })
+  public playlist: HasOne<typeof Playlist>
 }
