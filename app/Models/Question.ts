6750173d81b023d453dce49cb53a2f8eb4fa1d50
carloseduardodb <carloscangere@hotmail.com>
2021-07-01 18:26:47 -0300

    feat: add model question

--- app/Models/Question.ts
+++ app/Models/Question.ts
@@ -1,13 +1,31 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, HasMany, hasMany, HasOne, hasOne } from '@ioc:Adonis/Lucid/Orm'
+import Quiz from './Quiz'
+import Alternative from './Alternative'
 
 export default class Question extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public title: string
+
+  @column()
+  public quizId: number
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @hasOne(() => Quiz, {
+    localKey: 'quizId',
+  })
+  public quiz: HasOne<typeof Quiz>
+
+  @hasMany(() => Alternative, {
+    foreignKey: 'questionId',
+  })
+  public alternatives: HasMany<typeof Alternative>
 }
