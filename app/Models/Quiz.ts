5a20caf49e5fb5df8e3fb8d753a9624d98f849a5
carloseduardodb <carloscangere@hotmail.com>
2021-07-01 18:26:30 -0300

    feat: add model quiz

--- app/Models/Quiz.ts
+++ app/Models/Quiz.ts
@@ -1,13 +1,35 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, HasMany, hasMany, HasOne, hasOne } from '@ioc:Adonis/Lucid/Orm'
+import Playlist from './Playlist'
+import Question from './Question'
 
 export default class Quiz extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public title: string
+
+  @column()
+  public playlistId: number
+
+  @column()
+  public code: string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @hasOne(() => Playlist, {
+    localKey: 'playlistId',
+    foreignKey: 'id',
+  })
+  public playlist: HasOne<typeof Playlist>
+
+  @hasMany(() => Question, {
+    foreignKey: 'quizId',
+  })
+  public questions: HasMany<typeof Question>
 }
