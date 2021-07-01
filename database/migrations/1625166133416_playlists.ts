f5ee4cdb323c559d8192d5b1e27b503baa5f2198
carloseduardodb <carloscangere@hotmail.com>
2021-07-01 18:23:49 -0300

    feat(playlist.ts): add migration for model playlist

---
+++ database/migrations/1625166133416_playlists.ts
@@ -0,0 +1,27 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Playlists extends BaseSchema {
+  protected tableName = 'playlists'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name')
+      table
+        .integer('teacher_id')
+        .unsigned()
+        .references('id')
+        .inTable('teachers')
+        .onDelete('CASCADE')
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
