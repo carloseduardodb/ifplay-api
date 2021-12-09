8083c6ea6c439f0d2282a8f75e688f16f32460bb
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-09 01:30:41 -0300

    feat: add methods and more items in Response

--- app/Models/Response.ts
+++ app/Models/Response.ts
@@ -1,15 +1,22 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, hasOne, HasOne, scope } from '@ioc:Adonis/Lucid/Orm'
+import Teacher from './Teacher'
 
 export default class Response extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
   @column()
+  public student: string
+
+  @column()
   public playlistId: number
 
   @column()
   public question: number
+
+  @column()
+  public teacherId: number
 
   @column()
   public status: boolean
