9cdf293a7f26d61e2ac04498456c742cbc9d85bc
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-09 01:28:32 -0300

    feat: create methods in QuizzesController

--- app/Controllers/Http/QuizzesController.ts
+++ app/Controllers/Http/QuizzesController.ts
@@ -1,24 +1,24 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Quiz from '../../Models/Quiz'
 
 export default class QuizzesController {
-  public async index ({}: HttpContextContract) {
+  public async index({ params }: HttpContextContract) {
+    const quiz = await Quiz.find(params.id)
+    return quiz
   }
 
-  public async create ({}: HttpContextContract) {
+  public async create({}: HttpContextContract) {}
+
+  public async update({ params, request, response }: HttpContextContract) {
+    const { title } = await request.all()
+    const quiz = await Quiz.find(params.id)
+    if (quiz) {
+      quiz.title = title
+      await quiz.save()
+    }
+
+    return response.status(200).json(quiz)
   }
 
-  public async store ({}: HttpContextContract) {
-  }
-
-  public async show ({}: HttpContextContract) {
-  }
-
-  public async edit ({}: HttpContextContract) {
-  }
-
-  public async update ({}: HttpContextContract) {
-  }
-
-  public async destroy ({}: HttpContextContract) {
-  }
+  public async destroy({}: HttpContextContract) {}
 }
