be665817c3a1d7713a3cf2d5294ff247997fec8d
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-09 01:29:03 -0300

    feat: create methods in TeamsController

--- app/Controllers/Http/TeamsController.ts
+++ app/Controllers/Http/TeamsController.ts
@@ -1,24 +1,27 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Team from '../../Models/Team'
+import { v4 as uuidv4 } from 'uuid'
 
 export default class TeamsController {
-  public async index ({}: HttpContextContract) {
+  public async index({}: HttpContextContract) {
+    return await Team.all()
   }
 
-  public async create ({}: HttpContextContract) {
+  public async create({}: HttpContextContract) {}
+
+  public async store({ request, auth }: HttpContextContract) {
+    Team.create({
+      ...request.all(),
+      code: uuidv4(),
+      teacherId: auth.user!.id,
+    })
   }
 
-  public async store ({}: HttpContextContract) {
-  }
+  public async show({}: HttpContextContract) {}
 
-  public async show ({}: HttpContextContract) {
-  }
+  public async edit({}: HttpContextContract) {}
 
-  public async edit ({}: HttpContextContract) {
-  }
+  public async update({}: HttpContextContract) {}
 
-  public async update ({}: HttpContextContract) {
-  }
-
-  public async destroy ({}: HttpContextContract) {
-  }
+  public async destroy({}: HttpContextContract) {}
 }
