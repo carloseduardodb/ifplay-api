73050b7b083c80913c21606c2bd296f348269c49
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-09 01:28:15 -0300

    feat: create methods in QuestionsController

--- app/Controllers/Http/QuestionsController.ts
+++ app/Controllers/Http/QuestionsController.ts
@@ -1,24 +1,50 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Question from '../../Models/Question'
+import Alternative from '../../Models/Alternative'
 
 export default class QuestionsController {
-  public async index ({}: HttpContextContract) {
+  public async index({}: HttpContextContract) {}
+
+  public async create({}: HttpContextContract) {}
+
+  public async store({ auth, request }: HttpContextContract) {
+    Question.create({
+      title: request.all().title,
+      quizId: request.all().quizId,
+      teacherId: auth.user!.id,
+    }).then((question) => {
+      Alternative.create({
+        questionId: question.id,
+        isResponse: request.all().response.question1Rp,
+        title: request.all().question1,
+        teacherId: auth.user!.id,
+      })
+      Alternative.create({
+        questionId: question.id,
+        isResponse: request.all().question2Rp,
+        title: request.all().question2,
+        teacherId: auth.user!.id,
+      })
+      Alternative.create({
+        questionId: question.id,
+        isResponse: request.all().question3Rp,
+        title: request.all().question3,
+        teacherId: auth.user!.id,
+      })
+      Alternative.create({
+        questionId: question.id,
+        isResponse: request.all().question4Rp,
+        title: request.all().question4,
+        teacherId: auth.user!.id,
+      })
+    })
   }
 
-  public async create ({}: HttpContextContract) {
-  }
+  public async show({}: HttpContextContract) {}
 
-  public async store ({}: HttpContextContract) {
-  }
+  public async edit({}: HttpContextContract) {}
 
-  public async show ({}: HttpContextContract) {
-  }
+  public async update({}: HttpContextContract) {}
 
-  public async edit ({}: HttpContextContract) {
-  }
-
-  public async update ({}: HttpContextContract) {
-  }
-
-  public async destroy ({}: HttpContextContract) {
-  }
+  public async destroy({}: HttpContextContract) {}
 }
