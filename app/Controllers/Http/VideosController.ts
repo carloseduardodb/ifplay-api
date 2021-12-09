e78af7c0521af89fa424772afa211d6c0601cf9a
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-09 01:29:14 -0300

    feat: create methods in VideosController

--- app/Controllers/Http/VideosController.ts
+++ app/Controllers/Http/VideosController.ts
@@ -1,24 +1,26 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Video from '../../Models/Video'
+import CreateVideoValidator from '../../Validators/CreateVideoValidator'
 
 export default class VideosController {
-  public async index ({}: HttpContextContract) {
+  public async index({}: HttpContextContract) {}
+
+  public async create({}: HttpContextContract) {}
+
+  public async store({ auth, request }: HttpContextContract) {
+    const data = await request.validate(CreateVideoValidator)
+    const video = await Video.create({
+      ...data,
+      views: 0,
+      teacherId: auth.user!.id,
+    })
   }
 
-  public async create ({}: HttpContextContract) {
-  }
+  public async show({}: HttpContextContract) {}
 
-  public async store ({}: HttpContextContract) {
-  }
+  public async edit({}: HttpContextContract) {}
 
-  public async show ({}: HttpContextContract) {
-  }
+  public async update({}: HttpContextContract) {}
 
-  public async edit ({}: HttpContextContract) {
-  }
-
-  public async update ({}: HttpContextContract) {
-  }
-
-  public async destroy ({}: HttpContextContract) {
-  }
+  public async destroy({}: HttpContextContract) {}
 }
