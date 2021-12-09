ccd950bfa0f4566d555345fe3aca1c2acb318634
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-09 01:27:30 -0300

    feat: create AnswersController

--- app/Controllers/Http/AnswersController.ts
+++ app/Controllers/Http/AnswersController.ts
@@ -1,24 +1,26 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
+import Response from '../../Models/Response'
 
 export default class AnswersController {
-  public async index ({}: HttpContextContract) {
+  public async index({}: HttpContextContract) {}
+
+  public async countAll({ auth }: HttpContextContract) {
+    const count = await Database.from('responses').where('teacher_id', auth.user!.id).count({
+      count: '*',
+    })
+    return count
   }
 
-  public async create ({}: HttpContextContract) {
-  }
+  public async create({}: HttpContextContract) {}
 
-  public async store ({}: HttpContextContract) {
-  }
+  public async store({}: HttpContextContract) {}
 
-  public async show ({}: HttpContextContract) {
-  }
+  public async show({}: HttpContextContract) {}
 
-  public async edit ({}: HttpContextContract) {
-  }
+  public async edit({}: HttpContextContract) {}
 
-  public async update ({}: HttpContextContract) {
-  }
+  public async update({}: HttpContextContract) {}
 
-  public async destroy ({}: HttpContextContract) {
-  }
+  public async destroy({}: HttpContextContract) {}
 }
