18cbb65f2e20b042a141c28034b995bacd777282
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-09 01:32:46 -0300

    feat: add migratons questions

---
+++ database/migrations/1625166133414_questions.ts
@@ -0,0 +1,26 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Questions extends BaseSchema {
+  protected tableName = 'questions'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('title')
+      table.integer('quiz_id').unsigned().references('id').inTable('quizzes').onDelete('CASCADE')
+      table
+        .integer('teacher_id')
+        .unsigned()
+        .references('id')
+        .inTable('teachers')
+        .onDelete('CASCADE')
+        .notNullable()
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
