127052b8fcd9285aef2086fb0d8c31c190e2f0f3
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-12 17:27:54 -0300

    feat: update env ts with the .env data

--- env.ts
+++ env.ts
@@ -20,4 +20,6 @@ export default Env.rules({
   APP_KEY: Env.schema.string(),
   APP_NAME: Env.schema.string(),
   NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
+  MAILGUN_API_KEY: Env.schema.string(),
+  MAILGUN_DOMAIN: Env.schema.string(),
 })
